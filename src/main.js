f43eff8870f7e129e90114116816b2e2220d7471
Hardart <kaino@bk.ru>
2022-10-28 16:28:48 +0300

    change url for socket.io

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ import store from './store'
 import directives from '@/directives'
 import components from '@/components/UI'
 import { io } from 'socket.io-client'
+import config from './config'
 // loads the Icon plugin
 
-
-const socketIO = io('http://localhost:5500')
+const socketIO = io(config.baseURL)
 
 const app = createApp(App)
-components.forEach((comp) => app.component(comp.name, comp.self))
-directives.forEach((directive) => app.directive(directive.name, directive))
+components.forEach(comp => app.component(comp.name, comp.self))
+directives.forEach(directive => app.directive(directive.name, directive))
 
 app.provide('io', socketIO)
 app.use(store).use(router).mount('#app')
